b6f86f160fd4c1ccbd816d322137cb8c0a666f14
servoo <servettakmaz@gmail.com>
2021-10-02 11:03:44 +0200

    copy paste more

--- src/main/java/guru/springframework/domain/Notes.java
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -5,9 +5,38 @@ import javax.persistence.*;
 /**
  * Created by jt on 6/13/17.
  */
+@Entity
 public class Notes {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+	@OneToOne
     private Recipe recipe;
     private String recipeNotes;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	public Recipe getRecipe() {
+		return recipe;
+	}
+
+	public void setRecipe(final Recipe recipe) {
+		this.recipe = recipe;
+	}
+
+	public String getRecipeNotes() {
+		return recipeNotes;
+	}
+
+	public void setRecipeNotes(final String recipeNotes) {
+		this.recipeNotes = recipeNotes;
+	}
 }
--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -2,10 +2,18 @@ package guru.springframework.domain;
 
 import javax.persistence.*;
 
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
 /**
  * Created by jt on 6/13/17.
  */
+@Entity
 public class Recipe {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
     private String description;
     private Integer prepTime;
@@ -16,6 +24,11 @@ public class Recipe {
     private String directions;
     //todo add
     //private Difficulty difficulty;
+
+	@Lob
     private Byte[] image;
+
+    @OneToOne
+	@Cascade(CascadeType.ALL)
     private Notes notes;
 }
